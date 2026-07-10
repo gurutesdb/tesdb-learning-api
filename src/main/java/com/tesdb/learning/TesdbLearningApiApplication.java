c47af2b441253b2de05d5ad005b6d11b8c739aed
Guru <gurutesdb@gmail.com>
2026-07-10 19:06:40 +0530

    Initial commit - Spring Boot backend setup

---
+++ src/main/java/com/tesdb/learning/TesdbLearningApiApplication.java
@@ -0,0 +1,14 @@
+package com.tesdb.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesdbLearningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesdbLearningApiApplication.class, args);
+		//System.out.println("Welcome to E-Learning");
+	}
+
+}
---
+++ src/test/java/com/tesdb/learning/TesdbLearningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesdb.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesdbLearningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
